ba9ddf109038371ad1e28ecefecf0502099c55a2
Kasyene <kinga.mirkiewicz@gmail.com>
2018-11-24 19:42:00 +0100

    Player stats and level

--- Interfejsy/Assets/Scripts/Player.cs
+++ Interfejsy/Assets/Scripts/Player.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour {
 
     public int hp = 100;
     public int mana = 200;
+    public int[] stats;
+    public int level = 1;
+    public int statPointsSpend = 0;
+    public int statPointsToSpend = 0;
     public List<Ability> abilities;
-    static Player myInstance; 
+    static Player myInstance;
 
     public static Player Instance
     {
@@ -24,10 +29,27 @@ public class Player : MonoBehaviour {
     // Use this for initialization
     void Start () {
         abilities = new List<Ability>();
-	}
+        stats = new int[4];
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void LevelUp()
+    {
+        level += 1;
+        statPointsToSpend += 3;
+    }
+
+    public void AddToStat(int index)
+    {
+        if(statPointsToSpend != 0)
+        {
+            statPointsToSpend -= 1;
+            stats[index] += 1;
+        }
+    }
+
 }
