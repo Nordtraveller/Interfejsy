3c8ae1e56e9585c8a735b4d45890a3ae80365c1c
Piotr Morawski <piter.morawski@gmail.com>
2018-11-25 17:52:20 +0100

    +TextManager for text that depends on something in da game

---
+++ Interfejsy/Assets/Scripts/UI/TextManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+
+    [SerializeField] private Text statPointsToSpendText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        statPointsToSpendText.text = "Points: " + GetComponent<Player>().statPointsToSpend.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    statPointsToSpendText.text = "Points: " + GetComponent<Player>().statPointsToSpend.ToString();
+    }
+}
