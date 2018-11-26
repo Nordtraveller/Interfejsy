a504a0a200f9aa88f02a7febf74d4b8907a89ab0
Mateusz Nagórka <mnagorka96@gmail.com>
2018-11-26 21:57:03 +0100

    Add items (basics)

---
+++ Interfejsy/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum itemType
+{
+    None,
+
+    Sword,
+    Head,
+    Armor,
+    Shield
+}
+
+public class Item : MonoBehaviour {
+
+    [SerializeField] public ItemStats itemStats;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Interfejsy/Assets/Scripts/Items/ItemStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item/Stats", order = 1)]
+public class ItemStats : ScriptableObject
+{
+    public Sprite icon;
+
+    public int armor = 0;
+    public int damage = 0;
+    public itemType type = itemType.None;
+    public string itemName;
+    public string itemDescription;
+}
