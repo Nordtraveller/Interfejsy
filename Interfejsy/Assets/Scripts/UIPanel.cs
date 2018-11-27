bb33ff2832447db07b31cb4ffb279de3043055f8
Mateusz Nagórka <mnagorka96@gmail.com>
2018-11-27 09:21:02 +0100

    Displaying images on HUD (abilities & xbox icons)

---
+++ Interfejsy/Assets/Scripts/UIPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+
+    [SerializeField] private Image lbAbility;
+    [SerializeField] private Image xAbility;
+    [SerializeField] private Image aAbility;
+    [SerializeField] private Image bAbility;
+    [SerializeField] private Image yAbility;
+    [SerializeField] private Image rbAbility;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var player = GameObject.Find("GameManagers").GetComponent<Player>();
+
+        if (player)
+        {
+            lbAbility.sprite = player.currentAbilities[0].abilityStats.icon;
+            xAbility.sprite = player.currentAbilities[1].abilityStats.icon;
+            aAbility.sprite = player.currentAbilities[2].abilityStats.icon;
+            bAbility.sprite = player.currentAbilities[3].abilityStats.icon;
+            yAbility.sprite = player.currentAbilities[4].abilityStats.icon;
+            rbAbility.sprite = player.currentAbilities[5].abilityStats.icon;
+        }
+    }
+};
